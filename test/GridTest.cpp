106c9658dcd774a05b7f7236e4af6139d54896f6
Vince4x4 <vince4x4@gmail.com>
2017-10-11 16:35:55 +0200

    Completing tests for Grid evaluation methods

--- test/GridTest.cpp
+++ test/GridTest.cpp
@@ -119,7 +119,7 @@ BOOST_AUTO_TEST_CASE(grid_columnTransitions_test) {
 
 	g.fitPiece(triominos[0], Transformation(0, 0));
 	g.fitPiece(triominos[1], Transformation(0, 0));
-	/* Grid last three rows [expected]:
+	/* Grid last three rows:
 	2.	---xxx
 	1.	-----x
 	0.	----xx
@@ -127,10 +127,114 @@ BOOST_AUTO_TEST_CASE(grid_columnTransitions_test) {
 	BOOST_CHECK_EQUAL(10, g.columnTransitions());
 
 	g.fitPiece(triominos[0], Transformation(4, 2));
-	/* Grid last three rows [expected]:
+	/* Grid last three rows:
 	2.	---xxx
 	1.	xx---x
 	0.	x---xx
 	*/
 	BOOST_CHECK_EQUAL(12, g.columnTransitions());
+}
+
+BOOST_AUTO_TEST_CASE(grid_getCompleteLine_test) {
+	BOOST_CHECK_EQUAL(1023, Grid(10, 4).getCompleteLine());
+	BOOST_CHECK_EQUAL(15, Grid(4, 4).getCompleteLine());
+}
+
+BOOST_AUTO_TEST_CASE(grid_rowTransitions_test) {
+	Grid g(6, 6); // Small 6x6 grid
+	std::vector<Polyomino> triominos(getTriominos());
+
+	// Empty grid > no transitions since only non empty rows should be considered
+	BOOST_CHECK_EQUAL(0, g.rowTransitions());
+
+	g.fitPiece(triominos[0], Transformation(1, 0));
+	g.fitPiece(triominos[1], Transformation(4, 1));
+	/* Grid last three rows:
+	2.	-x----
+	1.	-x--x-
+	0.	-x-xx-
+	*/
+	BOOST_CHECK_EQUAL(16, g.rowTransitions());
+
+	g.fitPiece(triominos[1], Transformation(5, 1));
+	/* Grid last three rows:
+	2.	xx----
+	1.	xx--x-
+	0.	xx-xx-
+	*/
+	BOOST_CHECK_EQUAL(10, g.rowTransitions());
+}
+
+BOOST_AUTO_TEST_CASE(grid_cellars_test) {
+	Grid g(6, 6); // Small 6x6 grid
+	std::vector<Polyomino> triominos(getTriominos());
+
+	// Empty grid > no cellars possible
+	BOOST_CHECK_EQUAL(0, g.cellars());
+
+	g.fitPiece(triominos[0], Transformation(1, 0));
+	g.fitPiece(triominos[1], Transformation(0, 0));
+	/* Grid last three rows:
+	2.	---xxx
+	1.	----x-
+	0.	---xx-
+	*/
+	BOOST_CHECK_EQUAL(3, g.cellars());
+
+	g.fitPiece(triominos[1], Transformation(2, 0));
+	/* Grid last rows:
+	3.	-xxx--
+	2.	---xxx
+	1.	----x-
+	0.	---xx-
+	*/
+	BOOST_CHECK_EQUAL(9, g.cellars());
+}
+
+BOOST_AUTO_TEST_CASE(grid_wells_test) {
+	Grid g(6, 6); // Small 6x6 grid
+	std::vector<Polyomino> triominos(getTriominos());
+
+	// Empty grid > no wells possible
+	BOOST_CHECK_EQUAL(0, g.wells());
+
+	g.fitPiece(triominos[0], Transformation(1, 0));
+	g.fitPiece(triominos[1], Transformation(0, 0));
+	/* Grid last three rows: (wells are marked with O)
+	2.	---xxx
+	1.	----xO
+	0.	---xxO
+	*/
+	// A well of depth n will output the sum of i for i from 1 to n (thus n(n+1)/2)
+	BOOST_CHECK_EQUAL(3, g.wells()); // Here, we have only one well of depth=2 > 2+1=2*3/2=3
+
+	g.fitPiece(triominos[1], Transformation(4, 1));
+	/* Grid last rows:
+	2.	-x-xxx
+	1.	-x--x- // The central zone is not a well (for column=4) as there is an open position on the right
+	0.	-x-xx-
+	*/
+	// We have 3 wells, but the central one is not "complete" thus it will output 4 instead of 6 (3 + 1)
+	// Thus, we expect (from left to right) 6 + 4 + 3
+	BOOST_CHECK_EQUAL(13, g.wells());
+}
+
+BOOST_AUTO_TEST_CASE(grid_emptyBlocksDown_test) {
+	Grid g(6, 6); // Small 6x6 grid
+	std::vector<Polyomino> triominos(getTriominos());
+
+	// Empty grid > if we start at row n, we will get n+1 as a return value for each column
+	BOOST_CHECK_EQUAL(3, g.emptyBlocksDown(2, 2));
+	BOOST_CHECK_EQUAL(5, g.emptyBlocksDown(4, 0));
+
+	g.fitPiece(triominos[0], Transformation(1, 0));
+	g.fitPiece(triominos[1], Transformation(0, 0));
+	/* Grid last three rows:
+	2.	---xxx
+	1.	----x-
+	0.	---xx-
+	*/
+	BOOST_CHECK_EQUAL(0, g.emptyBlocksDown(2, 0));
+	BOOST_CHECK_EQUAL(2, g.emptyBlocksDown(1, 0));
+	BOOST_CHECK_EQUAL(1, g.emptyBlocksDown(1, 2));
 }
